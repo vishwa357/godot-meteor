b697b001e5478516432fe479f1ebf8bc59a0efc5
Vishwakant Mishra <vkmishra2050@gmail.com>
2024-05-03 10:12:51 +0530

    adding score

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -1,5 +1,8 @@
 extends CanvasLayer
 
+func _ready():
+	$CenterContainer/VBoxContainer/Label2.text = $CenterContainer/VBoxContainer/Label2.text + str(Global.score)
+
 func _process(_delta):
 	if Input.is_action_just_pressed("shoot"):
 		get_tree().change_scene_to_file("res://scenes/level.tscn")
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,6 +1,12 @@
 extends CanvasLayer
 
+var score : int = 0
 static var image = load("res://graphics/tiny-spaceships/tiny_ship5.png")
+
+func _ready():
+	score = 0
+	$MarginContainer/Label.text = str(score)
+
 func set_health(amount):
 	print("ui: " + str(amount))
 	for child in $MarginContainer2/HBoxContainer.get_children():
@@ -10,3 +16,9 @@ func set_health(amount):
 		text_rect.texture = image
 		text_rect.stretch_mode = TextureRect.STRETCH_KEEP
 		$MarginContainer2/HBoxContainer.add_child(text_rect)
+
+
+func _on_score_timer_timeout():
+	score += 1
+	$MarginContainer/Label.text = str(score)
+	Global.score = score
